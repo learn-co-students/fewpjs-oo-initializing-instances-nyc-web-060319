7cb366cf4244f8fdf725c2d0e66b9b985387dc80
TimeSmash <TimeSmash@github.com>
2019-08-02 22:27:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your code here
+
+
+
+//Remember that this refers to an instance in the class in this casey
+class Dog {
+    constructor(name, breed) {
+      this.name = name;
+      this.breed = breed;
+    }
+  }
+
+let bigFluffyDog1 = new Dog("Buzz", "greatPyrenees");
+let bigFluffyDog2 = new Dog("Woody", "labrador");
+
+// Create a class for Breakfast. Breakfast will have a constructor with a food and a drink.
+
+class Breakfast {
+    constructor(food, drink) {
+      this.food = food;
+      this.drink = drink;
+    }
+}
+// Create a class for Lunch. Lunch will have a constructor with a salad, a soup, and a drink.
+
+
+class Lunch {
+    constructor(salad, soup, drink) {
+      this.salad = salad;
+      this.soup = soup;
+      this.drink = drink;
+    }
+}
+
+// Create a class for Dinner. 
+// Since dinner is a little bit fancier, 
+// Dinner will have a constructor with salad, soup, entree, and dessert. 
+// Indicate that dessert is intended to be a private property by including an underscore (_) at the beginning.
+
+class Dinner {
+    constructor(salad, soup, entree, dessert) {
+      this.salad = salad;
+      this.soup = soup;
+      this.entree = entree;
+      this._dessert = dessert;
+    }
+}
